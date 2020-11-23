4d7cb244bf392878d5227f5f8782f4f7ea2f3aa0
Bernardo Smith <bernardostech@gmail.com>
2020-11-23 09:53:17 -0500

    Arquivos atualizados novo github repositorio

---
+++ exercises/2.1/README.md
@@ -0,0 +1,2 @@
+Esta pasta se refere ao nosso primeiro exercicio de Git e Github sobre configuracao, 
+conta, repositorio, merge, ect.
---
+++ exercises/2.1/sript.js
@@ -0,0 +1,10 @@
+let student = "euMesmo";
+let studentSkills = 10;
+
+//Eu sou Tryber e nunca desisto.
+
+
+Vou ter muito sucesso na programacao!
+
+
+console.log("Sucesso!");
