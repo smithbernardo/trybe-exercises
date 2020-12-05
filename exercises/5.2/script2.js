df16c5e5a8d3f506ad8dd3efafb8776c4b1f66fa
Bernardo Smith <bernardostech@gmail.com>
2020-12-05 17:03:03 -0500

    comecei mais alguns exercicios

---
+++ exercises/5.2/script2.js
@@ -0,0 +1,11 @@
+
+document.getElementById('elementoOndeVoceEsta');
+
+//let locationPai = document.getElementById('pai');
+//irmaoEleOndeVoceEsta = document.createElement('div');
+
+//locationPai.appendChild(irmaoEleOndeVoceEsta);
+let div = document.createElement('div');
+document.pai.appendChild(div);
+div.id = 'irmaoEleOndeVoceEsta';
+
