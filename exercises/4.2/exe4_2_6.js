3ec1e73adefcb9ef3fab65efc4501ce08b5e7d6d
bernardo smith <74397103+smithbernardo@users.noreply.github.com>
2020-12-08 23:10:46 -0500

    Delete exe4_2_6.js

--- exercises/4.2/exe4_2_6.js
+++
@@ -1,8 +0,0 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-quantidade = 0;
-for (valor = 0; valor < numbers.length; valor += 1) {
-    if (numbers[valor] % 2 != 0) {
-        quantidade += 1;
-    }
-}
-console.log('A quantidade de números ímpares é ' + quantidade)
