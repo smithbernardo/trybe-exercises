8d5d63c0c0d8997425285c0c23db81890366d792
bernardo smith <74397103+smithbernardo@users.noreply.github.com>
2020-12-08 23:10:21 -0500

    Delete exe4_2_3.js

--- exercises/4.2/exe4_2_3.js
+++
@@ -1,6 +0,0 @@
-let numbers = [5, 9, 3, 19, 70, 8, 100, 2, 35, 27];
-var soma = 0;
-for (valor = 0; valor < numbers.length; valor += 1) {
-    soma = soma + numbers[valor];
-}
-console.log(' A média é ' + soma / numbers.length)
