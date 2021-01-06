c907c517d89e35f8836ab2e71983875d8a8d36ee
Bernardo Smith <bernardostech@gmail.com>
2021-01-06 13:39:44 -0500

    exercicios

--- exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
+++ exercises/html-e-css-forms-flexbox-e-responsivo_1/script.js
@@ -1,1 +1,7 @@
-if 
+var field = document.getElementById('datepicker');
+var picker = new Pikaday({
+    onSelect: function(date) {
+        field.value = picker.toString();
+    }
+});
+field.parentNode.insertBefore(picker.el, field.nextSibling);
